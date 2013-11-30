e29aebafe71790fdcb30c2744ed57ba413b8e5c6
root <root@debian>
2013-11-30 14:14:00 +0200

    Add methods to helpers

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,9 @@
 module ArticlesHelper
+  def articles_count
+    Article.count
+  end
+
+  def time_now
+    Time.new.strftime '%F | %T'
+  end
 end
--- app/helpers/comments_helper.rb
+++ app/helpers/comments_helper.rb
@@ -1,2 +1,6 @@
 module CommentsHelper
+  def comments_count
+    article = Article.find params[:id]
+    article.comments.count
+  end
 end
